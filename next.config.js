f8f5a886538d6b9990f2a00a4b5c6c7b5ca6213a
SyedRaffiq01 <syedraffiq01@gmail.com>
2026-01-19 12:45:02 +0530

    Remove static export config - use serverless for API routes

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['maps.googleapis.com'],
-    unoptimized: true, // Required for static export
   },
-  // Uncomment below for static export (will disable API routes)
-  // output: 'export',
-  // trailingSlash: true,
 };
 
 module.exports = nextConfig;
